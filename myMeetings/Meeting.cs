fde17eadefe59e45ed856134a682a5f853815058
allilovee <alinalevanova2763@gmail.com>
2022-10-14 13:10:46 +0400

    Приложение с расписанием личных встреч

---
+++ myMeetings/Meeting.cs
@@ -0,0 +1,29 @@
+﻿namespace myMeetings
+{
+    public class Meeting
+    {
+        public readonly string Id;
+        public string Name;
+        public TimeSpan StartTime;
+        public TimeSpan EndTime;
+        public DateTime DateMeeting;
+        public DateTime? ReminderTime;
+        public bool NotificationSent = false;
+        //public DateTime DateAndTimeReminder
+        public Meeting (string name, TimeSpan startTime, TimeSpan endTime, DateTime dateMeeting, DateTime? reminderTime)
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Name = name;
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+            if(!(reminderTime == null))
+            {   
+                var reminderTimeNotNull = (DateTime)reminderTime;
+                this.ReminderTime = new DateTime(dateMeeting.Year, dateMeeting.Month, dateMeeting.Day);
+                var s = startTime.Add(-reminderTimeNotNull.TimeOfDay);
+                this.ReminderTime += s;
+            }
+            this.DateMeeting = dateMeeting;
+        }
+    }
+}
---
+++ myMeetings/MeetingManager.cs
@@ -0,0 +1,54 @@
+﻿namespace myMeetings
+{
+    public class MeetingManager
+    {
+        public List<Meeting> MeetingList = new List<Meeting>();
+        public bool TryAddMeeting(string name, TimeSpan startTime, TimeSpan endTime, DateTime dateMeeting, DateTime? reminderTime)
+        {
+            var canAdd = true;
+            foreach(Meeting meeting in MeetingList)
+            {
+                var crossed = meeting.DateMeeting == dateMeeting &&
+                    ((startTime >= meeting.StartTime && startTime <= meeting.EndTime) ||
+                    (endTime >= meeting.StartTime && endTime <= meeting.EndTime));
+                if (crossed)
+                {
+                    canAdd = false;
+                    break;
+                }
+            }
+            if(canAdd)
+            {
+                MeetingList.Add(new Meeting(name, startTime, endTime, dateMeeting, reminderTime));
+                return true;
+            }
+            return false;   
+        }
+
+        public void ChangeMeeting(string id, string name, TimeSpan startTime, TimeSpan endTime, DateTime dateMeeting, DateTime? reminderTime)
+        {
+            foreach(Meeting meeting in MeetingList)
+            {
+                if (meeting.Id == id)
+                {
+                    meeting.Name = name;
+                    meeting.StartTime = startTime;
+                    meeting.EndTime = endTime;
+                    meeting.DateMeeting = dateMeeting;
+                    meeting.ReminderTime = reminderTime;
+                }
+            }
+        }
+
+        public void DeleteMeeting(string id)
+        {
+            for (var i = MeetingList.Count - 1; i >= 0; i--) 
+            {
+                if (MeetingList[i].Id == id)
+                {
+                    MeetingList.Remove(MeetingList[i]);
+                }
+            }
+        }
+    }
+}
